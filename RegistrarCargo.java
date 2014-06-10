d360852e2b0ae43e19f9b7ca4edb5ae4df038692
josegarita <garitaj@gmail.com>
2014-06-10 00:57:37 -0600

    Create RegistrarCargo.java

---
+++ RegistrarCargo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package logica;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author JoseGarita
+ */
+public class RegistrarCargo {
+    
+    public static void RegisrtarCargo(String pNombre) throws ClassNotFoundException{
+        try {  
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/Funes?"+"user=root&password=sqlbases");
+            PreparedStatement pstat = (PreparedStatement) connection.prepareStatement("CALL insertarCargo(?)");
+            pstat.setString(1,pNombre);
+            ResultSet rs = pstat.executeQuery();  
+            
+            rs.close();  
+            pstat.close();  
+        } catch (SQLException e) {  
+            
+        } 
+		GUI.consultarPersona.nom=GUI.consultarPersona.pA=GUI.consultarPersona.sA=GUI.consultarPersona.ced=null;
+	}
+    
+}
+    
