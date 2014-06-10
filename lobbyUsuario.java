acfb4b7e46a1e0b80ebea9d1425c858d2b8123e9
Romigar <robertohr18@gmail.com>
2014-06-10 00:56:22 -0600

    Create lobbyUsuario.java

---
+++ lobbyUsuario.java
@@ -0,0 +1,102 @@
+package GUI;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class lobbyUsuario {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					lobbyUsuario window = new lobbyUsuario();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public lobbyUsuario() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 380, 243);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setTitle("Sesión de "+logica.logicaLobbyUsuario.usuarioActual);
+		
+		JLabel lblTitulo = new JLabel("Seleccione una opción");
+		lblTitulo.setBounds(112, 12, 176, 15);
+		frame.getContentPane().add(lblTitulo);
+		
+		JButton btnRegCat = new JButton("Categoría");
+		btnRegCat.setBounds(35, 84, 120, 25);
+		frame.getContentPane().add(btnRegCat);
+		
+		JButton btnEntidad = new JButton("Entidad");
+		btnEntidad.setBounds(35, 124, 117, 25);
+		frame.getContentPane().add(btnEntidad);
+		
+		JButton btnPersona = new JButton("Persona");
+		btnPersona.setBounds(35, 163, 117, 25);
+		frame.getContentPane().add(btnPersona);
+		
+		JLabel lblRegistrar = new JLabel("Registrar");
+		lblRegistrar.setBounds(65, 49, 70, 15);
+		frame.getContentPane().add(lblRegistrar);
+		
+		JLabel lblConsultar = new JLabel("Consultar");
+		lblConsultar.setBounds(257, 49, 70, 15);
+		frame.getContentPane().add(lblConsultar);
+		
+		JButton btnConCat = new JButton("Categoría");
+		btnConCat.setBounds(236, 84, 117, 25);
+		frame.getContentPane().add(btnConCat);
+		
+		JButton btnConPersona = new JButton("Persona");
+		btnConPersona.setBounds(236, 163, 117, 25);
+		frame.getContentPane().add(btnConPersona);
+		
+		btnRegCat.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				registrarCategoria.main(null);
+				frame.setVisible(false);
+			}
+		});
+		
+		btnPersona.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				registrarPersona.main(null);
+				frame.setVisible(false);
+			}
+		});
+		
+		btnConCat.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				consultarCategoria.main(null);
+				frame.setVisible(false);
+			}
+		});
+		
+	}
+}
